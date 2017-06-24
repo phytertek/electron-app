94d79152c47997d232acd626d4e5a1910ed07efd
Phytertek <ryan.phytertek@gmail.com>
2017-06-24 15:47:02 -0400

    save me

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import {Card, } from 'material-ui/Card'
+import TextField from 'material-ui/TextField'
+// import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
+        <AppBar title="Sign In" showMenuIconButton={false}/>
+        <TextField
+          hintText="Username"
+          floatingLabelText="Username"
+          fullWidth={true}
+        />
+        <TextField
+          hintText="Password"
+          floatingLabelText="Password"
+          type="password "
+          fullWidth={true}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './App';
+// TODO Reducer ##DONE
+// import rootReducer from './reducers'
 import './index.css';
-
+// const store = createStore(
+//   rootReducer, 
+//   applyMiddleware(thunk)
+// )
+injectTapEventPlugin()
 ReactDOM.render(
-  <App />,
+  // <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  // </Provider>
+  ,
   document.getElementById('root')
 );
